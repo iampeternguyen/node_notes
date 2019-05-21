910d79d2054b19bc33da48de47de94779c398f52
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-21 15:41:27 +0700

    adding commands using yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,46 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
+
 const getNotes = require('./notes');
 
-const command = process.argv[2];
+// customize yargs version
+yargs.version('1.0.1');
 
-if (command) {
-	console.log(command);
-}
+// Create add command
+yargs.command({
+	command: 'add',
+	describe: 'Add a new note',
+	handler: () => {
+		console.log('adding a new note');
+	},
+});
+
+// Create remove command
+yargs.command({
+	command: 'remove',
+	describe: 'Remove a new note',
+	handler: () => {
+		console.log('removing a new note');
+	},
+});
+
+// Create list command
+yargs.command({
+	command: 'list',
+	describe: 'List all notes',
+	handler: () => {
+		console.log('listing all notes');
+	},
+});
+
+// Create read command
+yargs.command({
+	command: 'read',
+	describe: 'Read a note. Specify title.',
+	handler: () => {
+		console.log('Reading note ____');
+	},
+});
+// add, remove, read, list
+
+console.log(yargs.argv);
