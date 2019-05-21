97ebcd3376f1ec6ef200a4b1792a1c2e9321445c
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-21 14:20:02 +0700

    learning: using npm packages

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,10 @@
+const validator = require('validator');
+
 const getNotes = require('./notes');
 
-console.log(getNotes());
+const msg = getNotes();
+console.log(msg);
+
+console.log(validator.isEmail('peter@github.com'));
+console.log(validator.isEmail('github.com'));
+console.log(validator.isURL('github.com'));
