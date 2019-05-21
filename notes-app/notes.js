8c007d810d2737238aa154775951cac2428343da
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-21 16:42:51 +0700

    can't add notes with same titles

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -3,15 +3,27 @@ const fs = require('fs');
 const add = (title, body) => {
 	const note = { title, body };
 	let notes = getNotes();
+	let duplicateNotes = notes.filter(note => {
+		return note.title === title;
+	});
 
-	notes.push(note);
+	if (duplicateNotes.length != 0) {
+		console.log('Title taken!');
+	} else {
+		notes.push(note);
+		save(notes);
+		console.log('New note added.');
+	}
+};
+
+const save = notes => {
 	fs.writeFileSync('notes.json', JSON.stringify(notes));
 };
 
 const list = () => {
 	let notes = getNotes();
 	notes.forEach((note, index) => {
-		console.log(`${index}. ${note.title}\n${note.body}`);
+		console.log(`${index + 1}. ${note.title}\n${note.body}`);
 	});
 };
 
@@ -19,10 +31,10 @@ const getNotes = () => {
 	let notes;
 	try {
 		notes = JSON.parse(fs.readFileSync('notes.json').toString());
+		return notes;
 	} catch (err) {
-		notes = [];
+		return [];
 	}
-	return notes;
 };
 
 module.exports = { add, list };
