1cfd8c36c2cef573c43f79270f5802522b33a50e
Peter Nguyen <ptnguyen1987@gmail.com>
2019-05-21 14:01:46 +0700

    reorganizing files

--- app.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-// fs.writeFileSync('notes.txt', 'My name is Peter.');
-
-fs.appendFileSync('notes.txt', 'This is some new text.');
---
+++ notes-app/app.js
@@ -0,0 +1,3 @@
+const name = 'Peter';
+
+console.log(name);
